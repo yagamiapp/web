26b07562d3671426784c9ef49f07f946f434ce37
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-09 00:43:10 -0400

    improve error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { page } from "$app/stores";
 	import logo from "../assets/yagami var.png";
+	import bg from "../assets/blue bg.png";
 </script>
 
 <svelte:head>
@@ -8,14 +9,29 @@
 </svelte:head>
 
 <div>
-	<img src={logo} alt="" />
+	<div class="bg-wrap">
+		<img class="bg" src={bg} alt="background" />
+	</div>
+	<img class="logo" src={logo} alt="logo" />
 	<h1>We encountered some trouble</h1>
 	<b>{$page.status} - {$page.error.message}</b>
 	<p>Make sure your typed in the URL correctly, or try again later.</p>
 </div>
 
 <style>
-	img {
+	.bg-wrap {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: -1;
+		width: 100%;
+		overflow: hidden;
+	}
+
+	img.bg {
+		min-width: 100%;
+	}
+	img.logo {
 		width: 500px;
 	}
 	div {
