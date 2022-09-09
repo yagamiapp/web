2b9efde48fbee59c3cdeee01830d627eb34a4468
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-09 00:11:53 -0400

    real working error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from "$app/stores";
+	import logo from "../assets/yagami var.png";
+</script>
+
+<svelte:head>
+	<title>{$page.status} - {$page.error.message}</title>
+</svelte:head>
+
+<div>
+	<img src={logo} alt="" />
+	<h1>We encountered some trouble</h1>
+	<b>{$page.status} - {$page.error.message}</b>
+	<p>Make sure your typed in the URL correctly, or try again later.</p>
+</div>
+
+<style>
+	img {
+		width: 500px;
+	}
+	div {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
