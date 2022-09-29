001c911290ada05485f9a0b658f0e577a1849e05
clxxiii <earthnoartisjusteh@gmail.com>
2022-09-29 15:56:10 -0400

    authentication fixes

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,30 +1,32 @@
 import prisma from "../lib/prisma";
 
 export async function load({ cookies, url }) {
+	let data = { origin: url.origin };
 	let session = cookies.get("yagami_session");
 
-	let user = await prisma.user.findFirst({
-		where: {
-			Sessions: {
-				some: {
-					id: session,
+	if (session) {
+		let user = await prisma.user.findFirst({
+			where: {
+				Sessions: {
+					some: {
+						id: session,
+					},
 				},
 			},
-		},
-	});
+		});
 
-	if (!user) {
-		cookies.delete("yagami_session");
-		return { origin: url.origin };
+		if (!user) {
+			cookies.delete("yagami_session");
+			return data;
+		}
+
+		delete user.access_token;
+		delete user.refresh_token;
+		delete user.expires_in;
+		delete user.type;
+
+		data.user = user;
 	}
 
-	delete user.access_token;
-	delete user.refresh_token;
-	delete user.expires_in;
-	delete user.type;
-
-	return {
-		user,
-		origin: url.origin,
-	};
+	return data;
 }
---
+++ src/routes/profile/+server.js
@@ -0,0 +1,26 @@
+import prisma from "../../lib/prisma";
+import { redirect } from "@sveltejs/kit";
+export async function GET({ cookies }) {
+	let session = cookies.get("yagami_session");
+
+	if (!session) {
+		throw redirect(302, "/");
+	}
+
+	let user = await prisma.user.findFirst({
+		where: {
+			Sessions: {
+				some: {
+					id: session,
+				},
+			},
+		},
+	});
+
+	if (!user) {
+		cookies.delete("yagami_session");
+		throw redirect(302, "/");
+	}
+
+	throw redirect(302, `/u/${user.id}`);
+}
