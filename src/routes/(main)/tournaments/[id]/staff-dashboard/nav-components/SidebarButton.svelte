4eaddf1ea19e511ede0234deea970c4982d59016
clxxiii <clxxiii@users.noreply.github.com>
2023-09-01 19:23:31 -0400

    super basic dashboard

---
+++ src/routes/(main)/tournaments/[id]/staff-dashboard/nav-components/SidebarButton.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import link_icon from './external-link.svg';
+	export let icon: string = link_icon;
+	export let href: string;
+	export let id: number;
+
+	let link = true;
+
+	if (href.startsWith('/')) {
+		href = `/tournaments/${id}/staff-dashboard${href}`;
+		link = false;
+	}
+</script>
+
+{#if !link}
+	<a {href}>
+		<slot>Button</slot>
+		<img src={icon} alt="" />
+	</a>
+{:else}
+	<a class="link" {href}>
+		<slot>Button</slot>
+		<img src={link_icon} alt="" />
+	</a>
+{/if}
+
+<style>
+	a {
+		position: relative;
+		display: block;
+		color: white;
+		text-decoration: none;
+		background: var(--bg3);
+		width: calc(100% - 10px * 2 - 15px * 2);
+		font-weight: bold;
+		height: 24px;
+		border-radius: 15px;
+		padding: 10px;
+		margin: 5px 15px;
+	}
+	a.link {
+		font-weight: normal;
+		background: transparent;
+	}
+	img {
+		position: absolute;
+		right: 10px;
+		width: 24px;
+		height: 24px;
+	}
+</style>
