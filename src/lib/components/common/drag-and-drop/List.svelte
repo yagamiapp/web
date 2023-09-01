133d08bff2959695cbfc5d68512732630c7f89a5
clxxiii <clxxiii@users.noreply.github.com>
2023-09-01 19:39:00 -0400

    (marginally) improve match page styles

--- src/lib/components/common/drag-and-drop/List.svelte
+++ src/lib/components/common/drag-and-drop/List.svelte
@@ -1,18 +1,18 @@
 <!-- Inspired by the Generic List Component example on 'svelte-dnd-action' -->
 <script lang="ts">
-	import {dndzone, type DndEvent} from 'svelte-dnd-action';
-	import {flip} from 'svelte/animate';
+	import { dndzone, type DndEvent } from 'svelte-dnd-action';
+	import { flip } from 'svelte/animate';
 
 	export let itemsData: { [index: string]: any }[];
 	export let itemComponent: ConstructorOfATypedSvelteComponent;
 	export let onDrop: Function;
-    // Type determines which lists are compatible with each other
-    export let type: string = itemComponent.name;
-	export let idPropertyName: string = "id";
+	// Type determines which lists are compatible with each other
+	export let type: string = itemComponent.name;
+	export let idPropertyName: string = 'id';
 	export let flipDurationMs: number = 300;
-    export let dropTargetStyle: Record<string, string> = {};
+	export let dropTargetStyle: Record<string, string> = {};
 	export let dropFromOthersDisabled: boolean = false;
-	
+
 	function handleConsider(e: CustomEvent<DndEvent<any>>) {
 		itemsData = e.detail.items;
 	}
@@ -21,15 +21,15 @@
 	}
 </script>
 
-<section 
-	use:dndzone={{ items: itemsData, type, flipDurationMs, dropTargetStyle, dropFromOthersDisabled }} 
-	on:consider={handleConsider} 
+<section
+	use:dndzone={{ items: itemsData, type, flipDurationMs, dropTargetStyle, dropFromOthersDisabled }}
+	on:consider={handleConsider}
 	on:finalize={handleFinalize}
 >
 	{#each itemsData as item (item[idPropertyName])}
-        <div animate:flip={{duration: flipDurationMs}}>
-            <svelte:component this={itemComponent} {item}/>		
-        </div>
+		<div animate:flip={{ duration: flipDurationMs }}>
+			<svelte:component this={itemComponent} {item} />
+		</div>
 	{/each}
 </section>
 
--- src/routes/(main)/tournaments/[id]/staff-dashboard/matches/+page.svelte
+++ src/routes/(main)/tournaments/[id]/staff-dashboard/matches/+page.svelte
@@ -1,165 +1,127 @@
 <script lang="ts">
-    import List from "$lib/components/common/drag-and-drop/List.svelte";
-	import DraggableTeam from "$lib/components/tournament-page/match/DraggableTeam.svelte";
-	import MatchController from "$lib/components/tournament-page/match/MatchController.svelte";
-    import type { ActionData, PageServerData } from "./$types";
-	import CompactMatchCard from "$lib/components/tournament-page/match/CompactMatchCard.svelte";
-	import { MatchStates } from "$lib/MatchStates";
-	import ToggleSwitch from "$lib/components/common/ToggleSwitch.svelte";
+	import List from '$lib/components/common/drag-and-drop/List.svelte';
+	import DraggableTeam from '$lib/components/tournament-page/match/DraggableTeam.svelte';
+	import MatchController from '$lib/components/tournament-page/match/MatchController.svelte';
+	import type { ActionData, PageServerData } from './$types';
+	import CompactMatchCard from '$lib/components/tournament-page/match/CompactMatchCard.svelte';
 
-    export let data: PageServerData;
-    export let form: ActionData;
-    let { tournamentName, tournamentColor, teams, rounds } = data;
-    let selectedMatch: db.MatchWithTeams | undefined = undefined;
-    $: hideFinishedMatches = false;
+	export let data: PageServerData;
+	export let form: ActionData;
+	let { tournamentName, teams, rounds } = data;
+	let selectedMatch: db.MatchWithTeams | undefined = undefined;
 
-    // Filter teams to only include those not scheduled for their next match
-    for (const round of rounds) {
-        for (const match of round.Match) {
-            // The teams are removed if the match isn't finished
-            if (match.state !== MatchStates.ARCHIVED && match.state !== MatchStates.MATCH_CLOSED) {
-                for (const teamInMatch of match.Teams) {
-                    // Remove this team from the teams list
-                    const index = teams.findIndex((team) => team.id == teamInMatch.Team.id);
-                    if (index != -1) {
-                        teams.splice(index, 1);
-                    }
-                }
-            }
-        }
-    }
+	function onDrop(updatedTeams: db.TeamWithMembersAndMatches[]) {
+		teams = updatedTeams;
+	}
 
-    function onDrop(updatedTeams: db.TeamWithMembersAndMatches[]) {
-        teams = updatedTeams;
-    }
+	$: if (form?.status == 200) {
+		selectedMatch = form.match;
 
-    if (form?.status == 200) {
-        selectedMatch = form.match;
-    }
-
+		// Update round match list
+		if (selectedMatch != undefined) {
+			rounds.find((round) => round.id == selectedMatch?.roundId)?.Match.push(selectedMatch);
+		}
+	}
 </script>
 
 <svelte:head>
-    <title>Matches - Staff View - {tournamentName}</title>
+	<title>Matches - Staff View - {tournamentName}</title>
 </svelte:head>
 
-<div class="wrapper">
-    <div class="matches">
-        <h1>Matches</h1>
+<div class="matches">
+	<h1>Matches</h1>
 
-        <div class="match-list">
-            {#key rounds}
-                {#each rounds as round (round.id)}
-                    <div class="round">
-                        <h2>{round.name}</h2>
-                        {#each round.Match as match (match.id)}
-                            {#if !( (match.state == MatchStates.ARCHIVED || match.state == MatchStates.MATCH_CLOSED) && hideFinishedMatches ) }
-                                <CompactMatchCard {match} selected={selectedMatch?.id == match.id}
-                                    on:select={() => selectedMatch = match}/>
-                            {/if}
-                        {/each}
-                    </div>
-                {/each}
-            {/key}
-        </div>
+	<div class="match-list">
+		{#key rounds}
+			{#each rounds as round (round.id)}
+				<div class="round">
+					<h2>{round.name}</h2>
+					{#each round.Match as match}
+						<CompactMatchCard
+							{match}
+							selected={selectedMatch?.id == match.id}
+							on:select={() => (selectedMatch = match)}
+						/>
+					{/each}
+				</div>
+			{/each}
+		{/key}
+	</div>
 
-        <MatchController {rounds} {form} bind:selectedMatch />
-    </div>
+	<MatchController {rounds} {form} bind:selectedMatch />
+</div>
 
-    <div class="idle-teams">
-        <div id="hide-finished-matches">
-            <p>Hide finished matches: </p>
-            <ToggleSwitch 
-                onColor={tournamentColor}
-                handleColor="var(--bg4)"
-                on:check={({ detail }) => {
-                    hideFinishedMatches = detail;
-                }}
-                checked={hideFinishedMatches}
-            />
-        </div>
+<div class="idle-teams">
+	<h1>Idle Teams</h1>
 
-        <h1>Idle Teams</h1>
-
-        <div class="team-list">
-            <List itemsData={teams} itemComponent={DraggableTeam} {onDrop} 
-                dropTargetStyle={{
-                    'border': '0.2rem dashed yellow',
-                    'border-radius': '0.3rem',
-                    'padding': '0.3rem'
-                }}
-            />
-        </div>
-    </div>
+	<div class="team-list">
+		<List
+			itemsData={teams}
+			itemComponent={DraggableTeam}
+			{onDrop}
+			dropTargetStyle={{
+				border: '0.2rem dashed yellow',
+				'border-radius': '0.3rem',
+				padding: '0.3rem'
+			}}
+		/>
+	</div>
 </div>
 
 <style>
-    .wrapper {
-        margin: 0;
-        display: flex;
-        flex-direction: row;
-        justify-content: stretch;
-        background-color: var(--bg3);
-    }
-    div {
-        margin: 0.4rem;
-    }
-    h1 {
-        margin-left: 3%;
-        margin-right: 3%;
-        border-bottom: 0.2rem solid var(--bg1);
-    }
+	:global(main) {
+		display: flex;
+		flex-direction: row;
+		justify-content: stretch;
+		background-color: var(--bg3);
+	}
+	div {
+		margin: 0.4rem;
+	}
+	h1 {
+		margin-left: 3%;
+		margin-right: 3%;
+		border-bottom: 0.2rem solid var(--bg1);
+	}
 
-    .matches {
-        width: 75%;
-        background-color: var(--bg3);
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
-    
-    .idle-teams {
-        border: 0.2rem solid var(--bg1);
-        width: 25%;
-        background-color: var(--bg4);
-    }
+	.matches {
+		width: 75%;
+		background-color: var(--bg3);
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
 
-    #hide-finished-matches {
-        margin-left: 3%;
-        margin-right: 3%;
-        background-color: var(--bg3);
-        width: 44%;
-        border-radius: 0.6rem;
+	.idle-teams {
+		border: 0.2rem solid var(--bg1);
+		width: 25%;
+		background-color: var(--bg4);
+	}
+	.team-list {
+		margin-left: 3%;
+		margin-right: 3%;
+		width: 94%;
+		padding: 0.2rem;
+	}
+	.team-list :global(section) {
+		display: absolute;
+		padding: 0.5rem;
+		height: 100vh;
+	}
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .team-list {
-        margin-left: 3%;
-        margin-right: 3%;
-        width: 94%;
-        padding: 0.2rem;
-    }
-    .team-list :global(section) {
-        display: absolute;
-        padding: 0.5rem;
-        height: 100vh;
-    }
-
-    .match-list {
-        align-self: stretch;
-        height: 60vh;
-        overflow: scroll;
-    }
-    .round {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
-    .round h2 {
-        width: 100%;
-        border-bottom: 0.2rem solid var(--bg2);
-    }
+	.match-list {
+		align-self: stretch;
+		height: 60vh;
+		overflow: scroll;
+	}
+	.round {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+	.round h2 {
+		width: calc(100% - 10px * 2);
+		margin-left: 10px;
+		border-bottom: 0.2rem solid var(--bg2);
+	}
 </style>
