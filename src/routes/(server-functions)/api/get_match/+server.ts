7150135244e3c0daf88674c86e5979c5d16fd2fb
JPW03 <03.jamie.w@googlemail.com>
2023-09-01 16:59:45 +0100

    Re-added BigInt.toJSON

--- src/routes/(server-functions)/api/get_match/+server.ts
+++ src/routes/(server-functions)/api/get_match/+server.ts
@@ -3,6 +3,11 @@ import prisma from '$lib/prisma';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
+// I hate javascript
+BigInt.prototype.toJSON = function () {
+	return this.toString();
+};
+
 export const GET: RequestHandler = async ({ url }) => {
 	const id = url.searchParams.get('id');
 
