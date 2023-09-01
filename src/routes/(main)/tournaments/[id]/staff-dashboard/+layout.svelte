d190fe90f13633e4da978ef09abaed3a4915a998
clxxiii <clxxiii@users.noreply.github.com>
2023-09-01 19:23:31 -0400

    super basic dashboard

--- src/routes/(main)/tournaments/[id]/staff-dashboard/+layout.svelte
+++ src/routes/(main)/tournaments/[id]/staff-dashboard/+layout.svelte
@@ -1,11 +1,67 @@
 <script lang="ts">
-    import type { LayoutServerData } from './$types';
+	import type { LayoutData } from './$types';
+	import yagami from '$lib/assets/icons/gradient.svg';
+	import SidebarButton from './nav-components/SidebarButton.svelte';
 
-    export let data: LayoutServerData;
+	import bracket from './nav-components/bracket.svg';
+	import dashboard from './nav-components/dashboard.svg';
+	import matches from './nav-components/matches.svg';
+	import settings from './nav-components/settings.svg';
+	import mappools from './nav-components/mappools.svg';
+
+	export let data: LayoutData;
+
+	const tournament_icon = data.tournament?.icon_url;
+	const id = data.tournament.id;
 </script>
 
-<slot />
+<nav class="sidebar" style="--color: {data.tournament.color}">
+	<div class="icons">
+		<img src={yagami} alt="yagami" />
+		<img src={tournament_icon} alt="yagami" />
+	</div>
+	<div class="buttons">
+		<SidebarButton {id} href="/" icon={dashboard}>Dashboard</SidebarButton>
+		<SidebarButton {id} href="/matches" icon={matches}>Matches</SidebarButton>
+		<SidebarButton {id} href="/bracket" icon={bracket}>Bracket</SidebarButton>
+		<SidebarButton {id} href="/mappools" icon={mappools}>Mappools</SidebarButton>
+		<SidebarButton {id} href="/settings" icon={settings}>Settings</SidebarButton>
+		<SidebarButton {id} href="./">Tournament Page</SidebarButton>
+	</div>
+</nav>
+<div class="content">
+	<slot />
+</div>
 
 <style>
-    
+	.sidebar {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 250px;
+		height: 100%;
+		background-color: var(--bg2);
+		/* z-index: -1; */
+	}
+	.content {
+		position: absolute;
+		top: 0;
+		left: 250px;
+		width: calc(100% - 250px);
+		min-height: 100%;
+	}
+	.icons {
+		width: 90%;
+		margin: auto;
+		display: flex;
+		gap: 10px;
+		justify-content: center;
+		border-bottom: solid 4px var(--color);
+	}
+	.icons img {
+		padding: 5px;
+		border-radius: 15px;
+		width: 50px;
+		height: 50px;
+	}
 </style>
