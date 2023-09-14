7f467b92c4faa994604e63ecd855907ec7fd16d3
JPW03 <82644778+JPW03@users.noreply.github.com>
2023-09-14 18:09:05 +0100

    Start time setting for matches (#37)

--- src/lib/components/tournament-page/match/MatchSettings.svelte
+++ src/lib/components/tournament-page/match/MatchSettings.svelte
@@ -5,6 +5,18 @@
 
     export let match: db.MatchWithTeams;
     export let rounds: db.RoundWithEverything[];
+
+    $: existingStartTime = match.start_time?.toISOString().substring(0, 16) ?? ""
+    $: newStartTime = existingStartTime;
+
+    function validateNotInThePast(event: KeyboardEvent) {
+        if (newStartTime.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/)) {
+            const startTimeAsDate = new Date(newStartTime);
+            if (startTimeAsDate < new Date()) {
+                alert("Warning!\nSetting the start time to the past will default to the current time.\n\nThis will trigger the Discord bot to prompt the players to start the match immediately");
+            }
+        }
+    }
 </script>
 
 <h2>Match {match.id}</h2>
@@ -17,9 +29,19 @@
         <label for="rounds">Round: </label>
         <select name="rounds" required>
             {#each rounds as round}
-                <option value={round.id}>{round.name}</option>
+                <option value={round.id} selected={round.id == match.roundId}>{round.name}</option>
             {/each}
-        </select>
+        </select><br>
+
+        <label for="date">Start Time (UTC):</label>
+        <!-- Type "datetime-local" input elements format the value as a string: "[YYYY]-[MM]-[DD]T[HH]:[MM]" -->
+        <!-- This is equivalent to the first 16 characters of an ISO date string -->
+        <input 
+            type="datetime-local"
+            name="start_time"
+            bind:value={newStartTime}
+            on:keyup={validateNotInThePast}
+        />
 
         <p>
             <button type="submit">Update</button>
